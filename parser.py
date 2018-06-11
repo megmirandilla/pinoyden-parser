ab7dad27eb352e09e80384ed4d8e5a5a060a7f91
Guest Admin <mamirandilla@up.edu.ph>
2018-06-11 10:34:42 +0800

    Extracted user_id and post text.

--- parser.py
+++ parser.py
@@ -2,14 +2,26 @@ import urllib.request
 import copy
 from bs4 import BeautifulSoup
 
-url = 'http://www.pinoyden.com.ph/index.php?topic=391263.0'
+data = {'user_id':'', 'message':'', 'quotes':''}
+quote = {'user_id':'', 'message':''}
+quote_list = []
+
+url = 'http://www.pinoyden.com.ph/index.php?topic=343063.0'
 response = urllib.request.urlopen(url)
 html = response.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 
-authors = soup.find_all("div",{"class":"poster"})
-posts = soup.find_all("div",{"class":"post"})
+users_block = soup.find_all("div",{"class":"poster"})
+post_block = soup.find_all("div",{"class":"post"})
 # print(len(authors))
 
+posts = []
+for block in post_block:
+	posts.append(block.text)
 
+users = []
+for block in users_block:
+	users.append(block.find("div",{"class":"bigtext"}).find("a").text) 
+
+# print(users)
